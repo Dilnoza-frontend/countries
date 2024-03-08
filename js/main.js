44bcce420a5b98398517dfd9f6129e295c36e20f
Dilnoza-frontend <dilnoza_matyakubova@yahoo.com>
2024-03-07 22:53:05 -0500

    Add dark-mode to local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+// const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
+
+// elSiteThemeToggler.addEventListener('click', function (){
+  // document.body.classList.toggle('dark-mode');
+// });
+
 const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
 
-elSiteThemeToggler.addEventListener('click', function (){
-  document.body.classList.toggle('dark-mode');
-});
+if (elSiteThemeToggler) {
+  elSiteThemeToggler.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    localStorage.setItem('elSiteThemeToggler', 'dark-mode');
+  });
+
+  if (localStorage.getItem('elSiteThemeToggler') === 'dark');
+
+  elSiteThemeToggler.addEventListener('click', function (){
+    document.body.classList.add('dark-mode');
+  });
+};
