d8e202d0629bdf2bc3ea94dcac24d60a3f52db6a
Dilnoza-frontend <dilnoza_matyakubova@yahoo.com>
2024-05-10 22:00:24 -0400

    Update js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-// const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
-
-// elSiteThemeToggler.addEventListener('click', function (){
-  // document.body.classList.toggle('dark-mode');
-// });
 
 const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
+
+
+
+  if (elSiteThemeToggler) {
+  (localStorage.getItem('elSiteThemeToggler') === 'dark-mode');
+
+  elSiteThemeToggler.addEventListener('click', function (){
+    document.body.classList.add('dark-mode');
+  });
+   };
 
 if (elSiteThemeToggler) {
   elSiteThemeToggler.addEventListener('click', function (evt) {
     evt.preventDefault();
 
     localStorage.setItem('elSiteThemeToggler', 'dark-mode');
-  });
-
-  if (localStorage.getItem('elSiteThemeToggler') === 'dark');
-
-  elSiteThemeToggler.addEventListener('click', function (){
-    document.body.classList.add('dark-mode');
-});
-
-};
+  })};
